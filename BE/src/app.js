d78c8ea396609bb75108e1b2e7f7b1f67811dffa
mehran sf <m.saghebi.dev@gmail.com>
2024-02-12 17:41:43 +0330

    [Running:fire:] the app is just running

--- BE/src/app.js
+++ BE/src/app.js
@@ -1,13 +1,34 @@
-import http from 'http';
-import { env, port, ip, apiRoot } from './config';
-import express from './services/express';
+import { env, port, apiRoot } from './config';
+import express from 'express';
 import api from './api';
+import cors from 'cors';
+import compression from 'compression';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import { errorHandler as queryErrorHandler } from 'querymen';
+import { errorHandler as bodyErrorHandler } from 'bodymen';
 
-const app = express(apiRoot, api);
-const server = http.createServer(app);
+const app = express();
 
-server.listen(port, ip, () => {
-  console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env);
+/* istanbul ignore next */
+if (env === 'production' || env === 'development') {
+  app.use(cors());
+  app.use(compression());
+  app.use(morgan('dev'));
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(apiRoot, api);
+app.use(queryErrorHandler());
+app.use(bodyErrorHandler());
+
+
+
+
+
+app.listen(5200, () => {
+  console.log('Express server listening on http://%s:%s, in %s mode', port, env);
 });
 
 export default app;
